b7dd4328387af218c994025959df1622f55f54ee
Jairo Yate <jyatem@gmail.com>
2025-04-21 18:35:41 -0500

    Ok

--- 02_Portafolio/Controllers/HomeController.cs
+++ 02_Portafolio/Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace _02_Portafolio.Controllers
 
             //return View(persona);
 
-            var proyectos = ObtenerProyectos();
+            var proyectos = ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View(modelo);
         }
@@ -45,23 +45,54 @@ namespace _02_Portafolio.Controllers
 
         private List<Proyecto> ObtenerProyectos()
         {
-            List<Proyecto> listadoProyectos = new List<Proyecto>();
+            //List<Proyecto> listadoProyectos = new List<Proyecto>();
 
-            listadoProyectos.Add(new Proyecto
-            {
-                Titulo = "Amazon",
-                Descripcion = "E-Commerce reaizado en ASP.NET Core",
-                Link = "https://amazom.com",
-                ImagenURL = ""
-            });
+            //listadoProyectos.Add(new Proyecto
+            //{
+            //    Titulo = "Amazon",
+            //    Descripcion = "E-Commerce reaizado en ASP.NET Core",
+            //    Link = "https://amazom.com",
+            //    ImagenURL = ""
+            //});
 
-            listadoProyectos.Add(new Proyecto
-            {
-                Titulo = "New York Times",
-                Descripcion = "Página de noticias en React",
-                Link = "https://nytimes.com",
-                ImagenURL = ""
-            });
+            //listadoProyectos.Add(new Proyecto
+            //{
+            //    Titulo = "New York Times",
+            //    Descripcion = "Página de noticias en React",
+            //    Link = "https://nytimes.com",
+            //    ImagenURL = ""
+            //});
+
+            List<Proyecto> listadoProyectos = new List<Proyecto> {
+                new Proyecto
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce reaizado en ASP.NET Core",
+                    Link = "https://amazom.com",
+                    ImagenURL = "/imagenes/amazon.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Página de noticias en React",
+                    Link = "https://nytimes.com",
+                    ImagenURL = "/imagenes/nyt.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades",
+                    Link = "https://reddit.com",
+                    ImagenURL = "/imagenes/reddit.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en línea para comprar videojuegos",
+                    Link = "https://store.steampowered.com",
+                    ImagenURL = "/imagenes/steam.png"
+                }
+            };
 
             return listadoProyectos;
         }
