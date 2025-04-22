4706da11230b32ad0492c1f36e8eb2db4ad97b8f
Jairo Yate <jyatem@gmail.com>
2025-04-21 19:47:02 -0500

    Ok

--- 02_Portafolio/Controllers/HomeController.cs
+++ 02_Portafolio/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using _02_Portafolio.Models;
+using _02_Portafolio.Servicios;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _02_Portafolio.Controllers
@@ -7,14 +8,19 @@ namespace _02_Portafolio.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRepositorioProyectos repositorioProyectos;
+        private readonly IConfiguration configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos, IConfiguration configuration)
         {
             _logger = logger;
+            this.repositorioProyectos = repositorioProyectos;
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            #region Código comentado
             //ViewBag.Nombre = "Jairo Yesid Yate";
             //ViewBag.Edad = 44;
             //return View("Index", "Jairo Yate Martínez");
@@ -25,9 +31,19 @@ namespace _02_Portafolio.Controllers
             //    Edad = 14
             //};
 
-            //return View(persona);
+            //return View(persona); 
+            #endregion
 
-            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var apellido = configuration.GetValue<string>("Apellido");
+
+            _logger.LogTrace("Este es un mensaje de trace");
+            _logger.LogDebug("Este es un mensaje de debug");
+            _logger.LogInformation("Este es un mensaje de information");
+            _logger.LogWarning("Este es un mensaje de warning");
+            _logger.LogError("Este es un mensaje de error");
+            _logger.LogCritical("Este es un mensaje de critical " + apellido);
+
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View(modelo);
         }
@@ -41,60 +57,6 @@ namespace _02_Portafolio.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        private List<Proyecto> ObtenerProyectos()
-        {
-            //List<Proyecto> listadoProyectos = new List<Proyecto>();
-
-            //listadoProyectos.Add(new Proyecto
-            //{
-            //    Titulo = "Amazon",
-            //    Descripcion = "E-Commerce reaizado en ASP.NET Core",
-            //    Link = "https://amazom.com",
-            //    ImagenURL = ""
-            //});
-
-            //listadoProyectos.Add(new Proyecto
-            //{
-            //    Titulo = "New York Times",
-            //    Descripcion = "Página de noticias en React",
-            //    Link = "https://nytimes.com",
-            //    ImagenURL = ""
-            //});
-
-            List<Proyecto> listadoProyectos = new List<Proyecto> {
-                new Proyecto
-                {
-                    Titulo = "Amazon",
-                    Descripcion = "E-Commerce reaizado en ASP.NET Core",
-                    Link = "https://amazom.com",
-                    ImagenURL = "/imagenes/amazon.png"
-                },
-                new Proyecto
-                {
-                    Titulo = "New York Times",
-                    Descripcion = "Página de noticias en React",
-                    Link = "https://nytimes.com",
-                    ImagenURL = "/imagenes/nyt.png"
-                },
-                new Proyecto
-                {
-                    Titulo = "Reddit",
-                    Descripcion = "Red social para compartir en comunidades",
-                    Link = "https://reddit.com",
-                    ImagenURL = "/imagenes/reddit.png"
-                },
-                new Proyecto
-                {
-                    Titulo = "Steam",
-                    Descripcion = "Tienda en línea para comprar videojuegos",
-                    Link = "https://store.steampowered.com",
-                    ImagenURL = "/imagenes/steam.png"
-                }
-            };
-
-            return listadoProyectos;
-        }
+        }        
     }
 }
---
+++ 02_Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,61 @@
+﻿using _02_Portafolio.Models;
+
+namespace _02_Portafolio.Servicios
+{
+    public class RepositorioProyectos : IRepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            //List<Proyecto> listadoProyectos = new List<Proyecto>();
+
+            //listadoProyectos.Add(new Proyecto
+            //{
+            //    Titulo = "Amazon",
+            //    Descripcion = "E-Commerce reaizado en ASP.NET Core",
+            //    Link = "https://amazom.com",
+            //    ImagenURL = ""
+            //});
+
+            //listadoProyectos.Add(new Proyecto
+            //{
+            //    Titulo = "New York Times",
+            //    Descripcion = "Página de noticias en React",
+            //    Link = "https://nytimes.com",
+            //    ImagenURL = ""
+            //});
+
+            List<Proyecto> listadoProyectos = new List<Proyecto> {
+                new Proyecto
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce reaizado en ASP.NET Core",
+                    Link = "https://amazom.com",
+                    ImagenURL = "/imagenes/amazon.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Página de noticias en React",
+                    Link = "https://nytimes.com",
+                    ImagenURL = "/imagenes/nyt.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades",
+                    Link = "https://reddit.com",
+                    ImagenURL = "/imagenes/reddit.png"
+                },
+                new Proyecto
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en línea para comprar videojuegos",
+                    Link = "https://store.steampowered.com",
+                    ImagenURL = "/imagenes/steam.png"
+                }
+            };
+
+            return listadoProyectos;
+        }
+    }
+}
